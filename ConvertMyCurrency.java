e06c33eea0cd5c7c20d190c0a55127a2d6fcc59b
Bao Ton <baothatthienton@yahoo.com>
2021-11-17 14:14:34 +0500

    Update ConvertMyCurrency.java

--- ConvertMyCurrency.java
+++ ConvertMyCurrency.java
@@ -1,136 +1,108 @@
-
- 
-import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 import java.text.DecimalFormat;
 
-public class ConvertMyCurrency {
-
-   public static void main(String[] args) {
- 
-		double amount, dollar, pound, code, euro, yen, ringgit, rupee;
- 
-		DecimalFormat f = new DecimalFormat("##.##");
- 
-		Scanner sc = new Scanner(System.in);
- 
-		System.out.println("This is a Currency Converter!");
- 
-		System.out.println("Please Choose the Currency You want to Convert ! ");
-		
-		System.out.println("1:RS \t2:$ \t3:Pound \n4:Euro \t5:Yen \t6:Ringgit ");
-		code = sc.nextInt();
-		
-		System.out.println("How much Money you want to convert ?");
-		amount = sc.nextFloat();
- 
-		// For amounts Conversion
-		if (code == 1) {
- 
-			dollar = amount / 70;
-			System.out.println("Your " + amount + " Rupee is : " + f.format(dollar) + " Dollar");
- 
-			pound = amount / 88;
-			System.out.println("Your " + amount + " Rupee is : " + f.format(pound) + " Pound");
- 
-			euro = amount / 80;
-			System.out.println("Your " + amount + " Rupee is : " + f.format(euro) + " Euro");
- 
-			yen = amount / 0.63;
-			System.out.println("Your " + amount + " Rupee is : " + f.format(yen) + " Yen");
- 
-			ringgit = amount / 16;
-			System.out.println("Your " + amount + " Rupee is : " + f.format(ringgit) + " ringgit");
-		} else if (code == 2) {
-			// For Dollar Conversion
- 
-			rupee = amount * 70;
-			System.out.println("Your " + amount + " Dollar is : " + f.format(rupee) + " Ruppes");
- 
-			pound = amount * 0.78;
-			System.out.println("Your " + amount + " Dollar is : " + f.format(pound) + " Pound");
- 
-			euro = amount * 0.87;
-			System.out.println("Your " + amount + " Dollar is : " + f.format(euro) + " Euro");
- 
-			yen = amount * 111.087;
-			System.out.println("Your " + amount + " Dollar is : " + f.format(yen) + " Yen");
- 
-			ringgit = amount * 4.17;
-			System.out.println("Your " + amount + " Dollar is : " + f.format(ringgit) + " ringgit");
-		} else if (code == 3) {
-			// For Pound Conversion
- 
-			rupee = amount * 88;
-			System.out.println("Your " + amount + " pound is : " + f.format(rupee) + " Ruppes");
- 
-			dollar = amount * 1.26;
-			System.out.println("Your " + amount + " pound is : " + f.format(dollar) + " Dollar");
- 
-			euro = amount * 1.10;
-			System.out.println("Your " + amount + " pound is : " + f.format(euro) + " Euro");
- 
-			yen = amount * 140.93;
-			System.out.println("Your " + amount + " pound is : " + f.format(yen) + " Yen");
- 
-			ringgit = amount * 5.29;
-			System.out.println("Your " + amount + " pound is : " + f.format(ringgit) + " ringgit");
-		} else if (code == 4) {
-			// For Euro Conversion
- 
-			rupee = amount * 80;
-			System.out.println("Your " + amount + " euro is : " + f.format(rupee) + " Ruppes");
- 
-			dollar = amount * 1.14;
-			System.out.println("Your " + amount + " euro is : " + f.format(dollar) + " Dollar");
- 
-			pound = amount * 0.90;
-			System.out.println("Your " + amount + " euro is : " + f.format(pound) + " Pound");
- 
-			yen = amount * 127.32;
-			System.out.println("Your " + amount + " euro is : " + f.format(yen) + " Yen");
- 
-			ringgit = amount * 4.78;
-			System.out.println("Your " + amount + " euro is : " + f.format(ringgit) + " ringgit");
-		} else if (code == 5) {
- 
-			// For Yen Conversion
- 
-			rupee = amount * 0.63;
-			System.out.println("Your " + amount + " yen is : " + f.format(rupee) + " Ruppes");
- 
-			dollar = amount * 0.008;
-			System.out.println("Your " + amount + " yen is : " + f.format(dollar) + " Dollar");
- 
-			pound = amount * 0.007;
-			System.out.println("Your " + amount + " yen is : " + f.format(pound) + " Pound");
- 
-			euro = amount * 0.007;
-			System.out.println("Your " + amount + " yen is : " + f.format(euro) + " Euro");
- 
-			ringgit = amount * 0.037;
-			System.out.println("Your " + amount + " yen is : " + f.format(ringgit) + " ringgit");
-		} else if (code == 6) {
-			// For Ringgit Conversion
- 
-			rupee = amount * 16.8;
-			System.out.println("Your " + amount + " ringgit is : " + f.format(rupee) + " Ruppes");
- 
-			dollar = amount * 0.239;
-			System.out.println("Your " + amount + " ringgit is : " + f.format(dollar) + " dollar");
- 
-			pound = amount * 0.188;
-			System.out.println("Your " + amount + " ringgit is : " + f.format(pound) + " pound");
- 
-			euro = amount * 0.209;
-			System.out.println("Your " + amount + " ringgit is : " + f.format(euro) + " euro");
- 
-			yen = amount * 26.63;
-			System.out.println("Your " + amount + " ringgit is : " + f.format(yen) + " yen");
-		} else {
-			System.out.println("Invalid input");
+public class ConvertMyCurrency extends JFrame implements ActionListener  {
+   private ButtonGroup checkBoxGroup;
+   JCheckBox rupee, usd, pound;
+   private JPanel panel;
+    private final JLabel result, error, result2;
+   JTextField txt; 
+   JButton button = new JButton("Convert");	
+   DecimalFormat f = new DecimalFormat("##.##");
+   
+   public Check() {
+      
+	  super("Currency Converer");
+      JLabel title = new JLabel("Test");
+	  
+	  panel = new JPanel(new GridLayout(10,0));
+      
+	  result = new JLabel();
+	  result2 = new JLabel();
+	  
+      error = new JLabel();
+	   
+	   txt=new JTextField();    
+	   
+	  title.setText("Please Select Currency\n");
+	  
+	  rupee = new JCheckBox("Rupee", false);
+      usd = new JCheckBox("USD", false);
+      pound = new JCheckBox("Pound", false);
+      checkBoxGroup = new ButtonGroup();
+      //add CheckBoxes to ButtonGroup
+      
+	  
+	  
+	  checkBoxGroup.add(rupee);
+      checkBoxGroup.add(usd);
+      checkBoxGroup.add(pound);
+	  panel.add(error);
+	  panel.add(txt); 
+	  panel.add(title);
+      panel.add(rupee);
+      panel.add(usd);
+      panel.add(pound);
+	  panel.add(button);
+	  button.addActionListener(this); 
+	  panel.add(result);
+	  panel.add(result2);
+      add(panel);
+      setSize(375, 250);
+	  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setLocationRelativeTo(null);
+      setVisible(true);
+   }
+	
+	
+   public void actionPerformed(ActionEvent e){
+        
+		if (txt.getText().equals("")){
+			error.setText(("Please Enter Amount"));
+			 
+		}else if(!pound.isSelected() && !rupee.isSelected() && !usd.isSelected())
+		{
+			error.setText(("Please Select Currency"));
+		}
+		else{
+			if(checkBoxGroup.getSelection().equals(pound.getModel())) {
+				if (e.getSource() == button) {
+				   double amount = Double.parseDouble(txt.getText()); //perform your operation
+				   double rupees = amount*218;
+				   double dollars  = amount*1.34;
+				   
+				   result.setText(("Your amount " + amount + " is " + f.format(dollars) + " in Dollar \n"));
+				   result2.setText(("Your amount " + amount + " is " + f.format(rupees) + " in Rupee"));
+				}
+			}
+			else if(checkBoxGroup.getSelection().equals(usd.getModel())) {
+				if (e.getSource() == button) {
+				   double amount = Double.parseDouble(txt.getText()); //perform your operation
+				   double pounds = amount/1.34;
+				   double rupees  = amount*170;
+				   
+				   result.setText(("Your amount " + amount + " is " + f.format(pounds) + " in Pound \n"));
+				   result2.setText(("Your amount " + amount + " is " + f.format(rupees) + " in Rupee"));
+				}
+			}
+			else if(checkBoxGroup.getSelection().equals(rupee.getModel())) {
+				if (e.getSource() == button) {
+				   double amount = Double.parseDouble(txt.getText()); //perform your operation 
+				   double dollars = amount*170;
+				   double pounds  = amount*218;
+				   result.setText(("Your amount " + amount + " is " + f.format(dollars) + " in Dollar \n"));
+				   result2.setText(("Your amount " + amount + " is " + f.format(pounds) + " in Pound"));
+				}
+			}
 		}
 		
-		System.out.println("Keep on using Currency Converter");
-	}
+		
+		
+    }
 
+   public static void main(String args[]) {
+      new Check();
+   }
 }
